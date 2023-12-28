64605c7953397639ab0621f6baa79b59babec707
ashraf56 <ashrafulfahim07@gmail.com>
2023-12-28 15:08:34 +0600

    code formated

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
 import styled from "styled-components";
-import { Banner, Card, Containers, Feilds,  Headertext } from "./components/styles/Global.styled"
+import { Banner, Card, Containers, Feilds, Headertext } from "./components/styles/Global.styled"
 import { useState } from "react";
 import "./App.css"
 import img from '../src/assets/Fingerprint-bro.png'
 import { FaEye, FaEyeSlash } from "react-icons/fa6";
-
 const Form = styled.form`
- 
   margin:auto;
 `;
-
 const FormControl = styled.div`
   margin-bottom: 1rem;
 `;
-
 const Label = styled.label`
   display: block;
   margin-bottom: 0.5rem;
@@ -23,18 +19,13 @@ font-style: normal;
 font-weight: 400;
 line-height: 110%; /* 19.8px */
 `;
-
 const Input = styled.input`
-min-width: ${props=> props.$w};
+min-width: ${props => props.$w};
 height:30px;
 border-radius: 8px;
 border: 1px solid rgba(4, 7, 47, 0.40);
 padding-left:5px;
-
- 
 `;
-
-
 const ForgotPasswordLink = styled.a`
   display: block;
   color: #F78719;
@@ -44,28 +35,20 @@ const ForgotPasswordLink = styled.a`
 const A = styled.a`
   color: #F78719;
   text-decoration: underline;
- 
   `;
- 
-const RandomDiv= styled.div`
+const RandomDiv = styled.div`
 display:flex;
 justify-content: space-between;
-
-
 `
-const RandomDiv2= styled.div`
+const RandomDiv2 = styled.div`
 display:flex;
 justify-content: space-between;
-
 `
-
 const Divforbutton = styled.div`
 text-align:center;
 `
-
 const SubmitButton = styled.button`
 width:300px;
-
   background-color: #007bff;
   color: #fff;
   border: none;
@@ -73,136 +56,92 @@ width:300px;
 background:  #1575A7;
   padding: 0.5rem 1rem;
   cursor: pointer;
-
-
   @media (max-width: 768px) {
     /* Styles for mobile devices */
     width:200px;
   }
-
   @media (min-width: 769px) and (max-width: 1023px) {
     width:200px;
   }
-
- 
-
 `;
-
-
-
 const ToggleButton = styled.button`
   background: transparent;
   border: none;
   font-size: 14px;
   cursor: pointer;
   margin-left: -30px;
-
 `;
 const Images = styled.img`
 width: 100%;
-
 `
-
-
 function App() {
-
-const [passwordVisible, setPasswordVisible] = useState(false);
-const [password, setPassword] = useState('');
-
-const togglePasswordVisibility = () => {
-  setPasswordVisible(!passwordVisible);
-};
-
-const handleSubmit = (e) => {
-  e.preventDefault(); 
-};
-
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [password, setPassword] = useState('');
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
-   
-     <Containers>
-<Banner>
-  <Images src={img}  />
- 
-</Banner>
-<Feilds>
-
-  <Card>
-     <Headertext>
-      LogIn
-     </Headertext>
-
-
-
-     <Form onSubmit={handleSubmit}>
-        <FormControl>
-          <Label>Login ID</Label>
-          <Input type="text" $w='100%' placeholder="Enter Login ID" required />
-        </FormControl>
-        <FormControl>
-          <Label>Password</Label>
-          <RandomDiv2>
-             <Input
-            type={passwordVisible ? 'text' : 'password'}
-            placeholder="password"
-            value={password}
-           $w='100%'
-            onChange={(e) => setPassword(e.target.value)}
-            required
-            maxLength={6}
-          />
-          <ToggleButton onClick={togglePasswordVisibility}>
-            {passwordVisible ? <FaEyeSlash/>:<FaEye/> }
-          </ToggleButton>
-          </RandomDiv2>
-
-         
-          
-        </FormControl>
-        <FormControl>
-        <RandomDiv>
-        <Label>  <input type="checkbox">
-          </input>
-          Remember Me</Label>
-          <ForgotPasswordLink>Change Password</ForgotPasswordLink>
-        </RandomDiv>
-        <Label>  <input type="checkbox">
-          </input>
-          Agree to <A>Terms & Conditions</A>     </Label>
-        </FormControl>
-      
-        <FormControl>
-          <Divforbutton>
-            <SubmitButton>Login</SubmitButton>
-          </Divforbutton>
-          
-        </FormControl>
-
-  <FormControl>
-  <Divforbutton>
-       <Label>  
-          Don’t have an account? 
-          <A>Register Here</A>     </Label>
-  </Divforbutton>
-     
-        </FormControl>
-
-
-      </Form>
-   
-
-    
-  </Card>
-
-
-
-
- 
-</Feilds>
-   
-     
-      </Containers>
-
+    <Containers>
+      <Banner>
+        <Images src={img} />
+      </Banner>
+      <Feilds>
+        <Card>
+          <Headertext>
+            LogIn
+          </Headertext>
+          <Form onSubmit={handleSubmit}>
+            <FormControl>
+              <Label>Login ID</Label>
+              <Input type="text" $w='100%' placeholder="Enter Login ID" required />
+            </FormControl>
+            <FormControl>
+              <Label>Password</Label>
+              <RandomDiv2>
+                <Input
+                  type={passwordVisible ? 'text' : 'password'}
+                  placeholder="password"
+                  value={password}
+                  $w='100%'
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                  maxLength={6}
+                />
+                <ToggleButton onClick={togglePasswordVisibility}>
+                  {passwordVisible ? <FaEyeSlash /> : <FaEye />}
+                </ToggleButton>
+              </RandomDiv2>
+            </FormControl>
+            <FormControl>
+              <RandomDiv>
+                <Label>  <input type="checkbox">
+                </input>
+                  Remember Me</Label>
+                <ForgotPasswordLink>Change Password</ForgotPasswordLink>
+              </RandomDiv>
+              <Label>  <input type="checkbox">
+              </input>
+                Agree to <A>Terms & Conditions</A>     </Label>
+            </FormControl>
+            <FormControl>
+              <Divforbutton>
+                <SubmitButton>Login</SubmitButton>
+              </Divforbutton>
+            </FormControl>
+            <FormControl>
+              <Divforbutton>
+                <Label>
+                  Don’t have an account?
+                  <A>Register Here</A>     </Label>
+              </Divforbutton>
+            </FormControl>
+          </Form>
+        </Card>
+      </Feilds>
+    </Containers>
   )
 }
-
 export default App
--- src/components/styles/Global.styled.js
+++ src/components/styles/Global.styled.js
@@ -1,34 +1,19 @@
 import styled from "styled-components";
-
-
-
-
-
-export const Containers =styled.section`
+export const Containers = styled.section`
 min-width:90%;
 min-height: 90vh;
 padding:20px;
 font-family: 'Poppins';
 display:flex;
 align-items: center;
-
 @media (max-width: 768px) {
     display:block
-     
   }
-
   @media (min-width: 769px) and (max-width: 1023px) {
-  
     display:block  
-   
 }
-
- 
-
 `;
-
-
-export const Banner= styled.section`
+export const Banner = styled.section`
 width:40%;
 height:100%;
 padding-left:40px;
@@ -36,31 +21,26 @@ padding-left:40px;
     width:100%;
     padding-left:0px;
   }
-
   @media (min-width: 769px) and (max-width: 1023px) {
    width:100%;
    padding-left:0px;
   }
 `;
-export const Feilds= styled.section`
+export const Feilds = styled.section`
 width:40%;
 height:100%;
 margin:auto;
 justify-content: center;
 align-items: center;
-
 @media (max-width: 768px) {
     /* Styles for mobile devices */
    width:100%;
-
   }
-
   @media (min-width: 769px) and (max-width: 1023px) {
     width:100%;
   }
-
 `;
-export const Card= styled.div`
+export const Card = styled.div`
 min-width:80%;
 align-items: center;
 border: 2px ;
@@ -71,22 +51,14 @@ padding:80px;
 border-radius: 12px;
 background: #FFF;
 box-shadow: 0px 10px 40px 0px rgba(0, 0, 0, 0.16);
-
 @media (max-width: 768px) {
     /* Styles for mobile devices */
     min-width:40%;
-
   }
-
   @media (min-width: 769px) and (max-width: 1023px) {
     min-width:50%;
   }
-
-
- 
-
 `;
-
 export const Headertext = styled.h1`
 color:  #04072F;
 text-align: center;
@@ -95,18 +67,10 @@ font-size: 48px;
 font-style: normal;
 font-weight: 700;
 line-height: 50%; 
-
-
-
   @media  (min-width: 1724px) {
-
     font-size: 68px 
-   
 }
-
 `;
-
-
 export const InputField = styled.input`
   padding: 10px;
   font-size: 16px;
